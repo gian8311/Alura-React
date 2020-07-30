622a91c7164fa918c895f6ec8edf42631c9dcd2d
Gian <gdass8000@gmail.com>
2020-07-30 16:37:16 -0300

    added proptypes

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,14 +1,15 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 function FormField({
   className,
-  type = "text",
+  type,
   name,
   id,
   value,
   onChange,
   label,
-  inputTag = "input",
+  inputTag,
 }) {
   if (inputTag === "input") {
     return (
@@ -37,6 +38,23 @@ function FormField({
       </div>
     )
   }
+}
+
+FormField.defaultProps = {
+  type: "text",
+  inputTag: "input",
+  value: "",
+  onChange: () => {},
+}
+FormField.propTypes = {
+  className: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  label: PropTypes.string.isRequired,
+  inputTag: PropTypes.string,
 }
 
 export default FormField
